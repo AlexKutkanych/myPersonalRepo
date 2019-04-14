f995da354f54e72fe90267f47b407bea6c4d31e9
AlexKutkanych <alexander.kutkanych@gmail.com>
2019-04-14 18:38:18 +0300

    added video

--- CSBO/src/components/VideoBlock/index.jsx
+++ CSBO/src/components/VideoBlock/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import VideoOverlay from '../VideoOverlay';
+import Video from '../../video/main-video.mp4';
 import './styles.scss';
 
 class VideoBlock extends Component {
@@ -7,10 +8,10 @@ class VideoBlock extends Component {
         return (
             <div class="video-block">
                 <div class="video-block__container">
-                    <video autoplay="" loop="" class="video-block__video" style={{width: '1543px', height: '867.938px'}}>
-                        <source type="video/mp4" src="vid/bg-video.mp4"/>
-                        <source type="video/webm" src="vid/bg-video.webm"/>
-                        <source type="video/ogg" src="vid/bg-video.ogv"/>
+                    <video autoplay="true" loop="true" class="video-block__video" style={{width: '100%', height: '867.938px'}}>
+                        <source type="video/mp4" src={Video}/>
+                        {/* <source type="video/webm" src="vid/bg-video.webm"/> */}
+                        {/* <source type="video/ogg" src="vid/bg-video.ogv"/> */}
                     </video>
                     <VideoOverlay />
                 </div>
